53284c65da25a111e834bfabfa45ae3e711e4a27
Alexandru Nedelcu <noreply@alexn.org>
2017-03-27 06:03:48 -0400

    Update Copyright

--- shared/src/main/scala_2.11/minitest/api/compat.scala
+++ shared/src/main/scala_2.11/minitest/api/compat.scala
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2014-2016 by Alexandru Nedelcu.
- * Some rights reserved.
+ * Copyright (c) 2014-2017 by its authors. Some rights reserved.
+ * See the project homepage at: https://github.com/monix/minitest
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
